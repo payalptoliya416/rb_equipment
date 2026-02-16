696c4f2d8d2738d2948998a23334c38c6b51b478
payalptoliya416 <payal.rising416@gmail.com>
2026-02-16 15:04:40 +0530

    16-02 RB head body script updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import localFont from "next/font/local";
 import ToastProvider from "@/components/common/ToastProvider";
 import { SettingsProvider } from "@/contexts/SettingsContext";
 import MetadataUpdater from "@/components/common/MetadataUpdater";
-
+import Script from "next/script";
 // ---------------- MONT FONT ----------------
 const mont = localFont({
   src: [
@@ -44,11 +44,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+       <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KF54G2N6');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+
       <body
         className={`${mont.variable} ${sfPro.variable} antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KF54G2N6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <SettingsProvider>
           <MetadataUpdater />
           {children}
